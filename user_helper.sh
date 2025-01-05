3ed0150cec6e012122ae4073fe32a4269c1b9672
Hosam <34396668+hosamn@users.noreply.github.com>
2025-01-05 22:55:30 +0200

    Update and rename bash project.txt to user_helper.sh

--- user_helper.sh
+++ user_helper.sh
@@ -4,15 +4,15 @@ options=("Add User" "Delete User" "Add Group" "Delete Group" "List User Details"
 
 
 while true; do
-  
-    choice=$(zenity --list \
-                    --title="System Admin helper tool" \
-                    --column="Options" "${options[@]}" \
-	    	    --width=400 \
-                    --height=400 \
-                    --auto-scroll)
 
-    
+    choice=$(zenity --list \
+        --title="System Admin helper tool" \
+        --column="Options" "${options[@]}" \
+        --width=400 \
+        --height=400 \
+    --auto-scroll)
+
+
     if [[ $? -ne 0 ]]; then
         echo "No option selected."
         exit 1
@@ -33,7 +33,7 @@ while true; do
             else
                 echo "Operation canceled."
             fi
-            ;;
+        ;;
         "Delete User")
             username=$(zenity --entry --title="Delete User" --text="Enter username to delete:")
             if [[ $? -eq 0 ]]; then
@@ -45,7 +45,7 @@ while true; do
             else
                 echo "Operation canceled."
             fi
-            ;;
+        ;;
         "Add Group")
             groupname=$(zenity --entry --title="Add Group" --text="Enter group name:")
             if [[ $? -eq 0 ]]; then
@@ -57,7 +57,7 @@ while true; do
             else
                 echo "Operation canceled."
             fi
-            ;;
+        ;;
         "Delete Group")
             groupname=$(zenity --entry --title="Delete Group" --text="Enter group name to delete:")
             if [[ $? -eq 0 ]]; then
@@ -69,29 +69,29 @@ while true; do
             else
                 echo "Operation canceled."
             fi
-            ;;
+        ;;
         "List User Details")
-           username=$(zenity --entry --title="List user details" --text="Enter username to list its details:")
+            username=$(zenity --entry --title="List user details" --text="Enter username to list its details:")
             if [[ $? -eq 0 ]]; then
                 if sudo id $username 1>user-details 2>/dev/null; then
-			zenity --info --title="User $username details" --text="$(cat user-details) ."
+                    zenity --info --title="User $username details" --text="$(cat user-details) ."
                 else
                     zenity --error --title="Error" --text="Failed to list detail for $username. Please check if the user exists or try again later."
                 fi
             else
                 echo "Operation canceled."
             fi
-	    ;;
+        ;;
         "Change User Password")
             username=$(zenity --entry --title="Change User Password" --text="Enter username:")
             password=$(zenity --entry --title="Change User Password" --text="Enter new password:")
-	    if echo $password | passwd --stdin $username; then
+            if echo $password | passwd --stdin $username; then
                 zenity --info --title="Change User: $(username) Password" --text="password changed successfully."
 
             else
                 zenity --error --title="Error" --text="Failed to change password for $username. Please check if the user exists or try again later."
             fi
-            ;;
+        ;;
         "Lock Account")
             username=$(zenity --entry --title="Lock Account" --text="Enter username to lock:")
             if sudo usermod --lock $username; then
@@ -99,7 +99,7 @@ while true; do
             else
                 zenity --error --title="Error" --text="Failed to lock account for $username. Please check if the user exists or try again later."
             fi
-            ;;
+        ;;
         "Unlock Account")
             username=$(zenity --entry --title="Unlock Account" --text="Enter username to unlock:")
             if sudo usermod --unlock $username; then
@@ -107,31 +107,31 @@ while true; do
             else
                 zenity --error --title="Error" --text="Failed to unlock account for $username. Please check if the user exists or try again later."
             fi
-            ;;
-	"Update Username")
+        ;;
+        "Update Username")
             username=$(zenity --entry --title="Update Account" --text="Enter username to update:")
-	    newusername=$(zenity --entry --title="Update Account" --text="Enter new username:")
+            newusername=$(zenity --entry --title="Update Account" --text="Enter new username:")
             if sudo usermod -l $newusername $username; then
                 zenity --info --title="Account Updated" --text="Account updated successfully."
             else
                 zenity --error --title="Error" --text="Failed to update account for $username. Please check if the user exists or try again later."
             fi
-            ;;
-	"Disable Account")
+        ;;
+        "Disable Account")
             username=$(zenity --entry --title="Disable Account" --text="Enter username to disable:")
             if sudo  passwd -l $username; then
                 zenity --info --title="Account Updated" --text="Account updated successfully."
             else
                 zenity --error --title="Error" --text="Failed to update account for $username. Please check if the user exists or try again later."
             fi
-            ;;
+        ;;
 
         "Exit")
             echo "Exiting the program."
             exit 0
-            ;;
+        ;;
         *)
             echo "Invalid option"
-            ;;
+        ;;
     esac
 done
